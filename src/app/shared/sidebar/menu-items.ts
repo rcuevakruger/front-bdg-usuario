a6c7cf1e0bf4fa5f1f01b1eb259471d62cf27212
cuevrob <rcueva@kruger.com.ec>
2018-10-15 17:16:13 -0500

    added dashboard and changed paths

--- src/app/shared/sidebar/menu-items.ts
+++ src/app/shared/sidebar/menu-items.ts
@@ -5,7 +5,15 @@ export const ROUTES: RouteInfo[] = [
         path: '', title: 'Personal', icon: '', class: 'nav-small-cap', label: '', labelClass: '', extralink: true, submenu: []
     },
     {
-        path: '/component/Billetera', title: 'Billetera', icon: 'mdi mdi-gauge', class: '', label: '', labelClass: '', extralink: false, submenu: []
+        path: '/dashboards/dashboard-components/Billetera', title: 'Billetera', icon: 'mdi mdi-gauge', class: '', label: '', labelClass: '', extralink: false, submenu: []
+    },
+    {
+        path: '', title: 'Dashboards', icon: 'mdi mdi-gauge', class: 'has-arrow', label: '4', labelClass: 'label label-rouded label-themecolor pull-right', extralink: false,
+        submenu: [
+            { path: '/dashboard/dashboard1', title: 'Usuario', icon: '', class: '', label: '', labelClass: '', extralink: false, submenu: [] },
+            { path: '/dashboard/dashboard2', title: 'Classic', icon: '', class: '', label: '', labelClass: '', extralink: false, submenu: [] },
+            { path: '/dashboard/dashboard3', title: 'Analytical', icon: '', class: '', label: '', labelClass: '', extralink: false, submenu: [] },
+        ]
     },
     {
         path: '', title: 'Transacciones', icon: '', class: 'nav-small-cap', label: '', labelClass: '', extralink: true, submenu: []
@@ -32,7 +40,6 @@ export const ROUTES: RouteInfo[] = [
             { path: '/component/timepicker', title: 'Timepicker', icon: '', class: '', label: '', labelClass: '', extralink: false, submenu: [] },
             { path: '/component/buttons', title: 'Button', icon: '', class: '', label: '', labelClass: '', extralink: false, submenu: [] },
             { path: '/component/cards', title: 'Card', icon: '', class: '', label: '', labelClass: '', extralink: false, submenu: [] },
-            { path: '/component/Billetera', title: 'Billetera', icon: '', class: '', label: '', labelClass: '', extralink: false, submenu: [] },
         ]
     }, */
     /* {
