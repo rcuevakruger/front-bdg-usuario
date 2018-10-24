6203b837ec5c5d6aa36934b0f393becb37d8fae6
cuevrob <rcueva@kruger.com.ec>
2018-10-24 17:09:46 -0500

    agregar archivo - Roberto Cueva

--- src/app/component/CambiarMillas.1/CambiarMillas.service.ts
+++ src/app/component/CambiarMillas.1/CambiarMillas.service.ts
@@ -15,36 +15,37 @@
 import { Injectable } from '@angular/core';
 import { DataService } from '../../data.service';
 import { Observable } from 'rxjs/Observable';
-import { CobrarMillas } from '../../com.kruger.millas.transacciones';
+import { CambiarMillas } from '../../com.kruger.millas.transacciones';
 import 'rxjs/Rx';
 
 // Can be injected into a constructor
 @Injectable()
-export class CobrarMillasService {
+export class CambiarMillasService {
 
-  private NAMESPACE = 'CobrarMillas';
+  private NAMESPACE = 'CambiarMillas';
 
-  constructor(private dataService: DataService<CobrarMillas>) {
+  constructor(private dataService: DataService<CambiarMillas>) {
   };
 
-  public getAll(): Observable<CobrarMillas[]> {
+  public getAll(): Observable<CambiarMillas[]> {
       return this.dataService.getAll(this.NAMESPACE);
   }
 
-  public getTransaction(id: any): Observable<CobrarMillas> {
+  public getTransaction(id: any): Observable<CambiarMillas> {
     return this.dataService.getSingle(this.NAMESPACE, id);
   }
 
-  public addTransaction(itemToAdd: any): Observable<CobrarMillas> {
+  public addTransaction(itemToAdd: any): Observable<CambiarMillas> {
     return this.dataService.add(this.NAMESPACE, itemToAdd);
   }
 
-  public updateTransaction(id: any, itemToUpdate: any): Observable<CobrarMillas> {
+  public updateTransaction(id: any, itemToUpdate: any): Observable<CambiarMillas> {
     return this.dataService.update(this.NAMESPACE, id, itemToUpdate);
   }
 
-  public deleteTransaction(id: any): Observable<CobrarMillas> {
+  public deleteTransaction(id: any): Observable<CambiarMillas> {
     return this.dataService.delete(this.NAMESPACE, id);
   }
 
 }
+
