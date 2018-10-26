604aeb4ab579f77827d94a63ef71c80a5d481178
cuevrob <rcueva@kruger.com.ec>
2018-10-26 09:45:43 -0500

    deleted login 2 component path - Roberto Cueva

--- src/app/authentication/authentication.routing.ts
+++ src/app/authentication/authentication.routing.ts
@@ -3,7 +3,6 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './404/not-found.component';
 import { LockComponent } from './lock/lock.component';
 import { LoginComponent } from './login/login.component';
-import { Login2Component } from './login2/login2.component';
 import { SignupComponent } from './signup/signup.component';
 import { Signup2Component } from './signup2/signup2.component';
 
@@ -19,9 +18,6 @@ export const AuthenticationRoutes: Routes = [
     }, {
       path: 'login',
       component: LoginComponent
-    }, {
-      path: 'login2',
-      component: Login2Component
     }, {
       path: 'signup',
       component: SignupComponent
