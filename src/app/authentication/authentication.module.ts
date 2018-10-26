affaa3f397916564709738d24e14b779377b92f0
cuevrob <rcueva@kruger.com.ec>
2018-10-26 09:45:27 -0500

    deleted login 2 component import - Roberto Cueva

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -7,7 +7,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NotFoundComponent } from './404/not-found.component';
 import { LockComponent } from './lock/lock.component';
 import { LoginComponent } from './login/login.component';
-import { Login2Component } from './login2/login2.component';
 import { SignupComponent } from './signup/signup.component';
 import { Signup2Component } from './signup2/signup2.component';
 
@@ -28,7 +27,6 @@ import { GeneralService } from '../dashboards/dashboard-components/services/Gene
     LoginComponent,
     SignupComponent,
     LockComponent,
-    Login2Component,
     Signup2Component
   ],
   providers:[GeneralService]
