354b8663adf9b6aec08cf9511671d3841fb91d70
cuevrob <rcueva@kruger.com.ec>
2018-10-26 09:49:04 -0500

    deleted import and declarations for useless components - Roberto Cueva

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -13,21 +13,6 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 
 import { ComponentsRoutes } from './component.routing';
-import { NgbdpregressbarBasic } from './progressbar/progressbar.component';
-import { NgbdpaginationBasic } from './pagination/pagination.component';
-import { NgbdAccordionBasic } from './accordion/accordion.component';
-import { NgbdAlertBasic } from './alert/alert.component';
-import { NgbdCarouselBasic } from './carousel/carousel.component';
-import { NgbdDatepickerBasic } from './datepicker/datepicker.component';
-import { NgbdDropdownBasic } from './dropdown-collapse/dropdown-collapse.component';
-import { NgbdModalBasic } from './modal/modal.component';
-import { NgbdPopTooltip } from './popover-tooltip/popover-tooltip.component';
-import { NgbdratingBasic } from './rating/rating.component';
-import { NgbdtabsBasic } from './tabs/tabs.component';
-import { NgbdtimepickerBasic } from './timepicker/timepicker.component';
-import { NgbdtypeheadBasic } from './typehead/typehead.component';
-import { CardsComponent } from './card/card.component';
-import { ButtonsComponent } from './buttons/buttons.component';
 import { BilleteraComponent } from "./Billetera/Billetera.component";
 import { CobrarMillasComponent } from "./CobrarMillas/CobrarMillas.component";
 import { AcreditarMillasComponent } from "./AcreditarMillas/AcreditarMillas.component";
@@ -52,21 +37,6 @@ import { PropietarioComponent } from "./Propietario/Propietario.component";
     // AppRoutingModule
   ],
   declarations: [
-    NgbdpregressbarBasic,
-    NgbdpaginationBasic,
-    NgbdAccordionBasic,
-    NgbdAlertBasic,
-    NgbdCarouselBasic,
-    NgbdDatepickerBasic,
-    NgbdDropdownBasic,
-    NgbdModalBasic,
-    NgbdPopTooltip,
-    NgbdratingBasic,
-    NgbdtabsBasic,
-    NgbdtimepickerBasic,
-    NgbdtypeheadBasic,
-    CardsComponent,
-    ButtonsComponent,
     BilleteraComponent,
     CobrarMillasComponent,
     AcreditarMillasComponent,
